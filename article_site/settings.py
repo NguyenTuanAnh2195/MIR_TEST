42b90d406b94723367f3ac54ff086da3fa7c3a8d
Tuan Anh Nguyen <anhnt20101995@gmail.com>
2023-11-27 19:45:24 +0700

    add email sending functionality

--- article_site/settings.py
+++ article_site/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_DESTINATION = os.getenv("EMAIL_DESTINATION")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
